68334a7a08550bd9fce680e2e41ac898b4243c0a
Tung <tungtopro123@gmail.com>
2025-06-13 02:04:38 +0700

    btjs

---
+++ day7/dcd.js
@@ -0,0 +1,15 @@
+function run(a) {
+    a *= 2;
+    console.log(a);
+    a += 9;
+    console.log(a);
+    a -= 4;
+    console.log(a);
+    a /= 3;
+    console.log(a);
+    a -= 3;
+    console.log(a);
+}
+
+run(5);
+
